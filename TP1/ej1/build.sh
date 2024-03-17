9f422f381fff2a29cdaac4d2eee8f78f521807bf
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-17 12:47:36 -0300

    documenta pasos con los que debería contar el pipeline

--- TP1/ej1/build.sh
+++ TP1/ej1/build.sh
@@ -1,2 +1,10 @@
-# Crea imagen de docker
+# 1. Crea imagen de docker
 docker build -t tcp-server .
+
+# 2. Docker login
+# 3. Push de la imagen a dockerhub
+# 4. Conexion a servidor remoto por ssh
+# 5. Pull de la imagen de dockerhub
+# 6. Detener el contenedor si esta corriendo
+# 7. Eliminar el contenedor
+# 8. Correr el contenedor con la nueva imagen
