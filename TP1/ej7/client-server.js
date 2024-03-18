70b7563f17ab53756222e2c567ac9367dabf47c8
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-18 11:14:17 -0300

    feat: agrega estructura base para HIT 7

---
+++ TP1/ej7/client-server.js
@@ -0,0 +1,117 @@
+const net = require('net');
+const http = require('http');
+
+// Obtener argumentos de la línea de comandos
+const args = process.argv.slice(2);
+if (args.length !== 2) {
+	console.log(
+		'Uso: node program.js <ip_servidor_registro> <puerto_servidor_registro> '
+	);
+	process.exit(1);
+}
+
+const serverAddress = { host: 'localhost', port: 0 };
+const remoteAddress = { host: args[0], port: parseInt(args[1]) };
+
+// Crear servidor
+const server = net.createServer((socket) => {
+	console.log(
+		'Cliente conectado desde',
+		socket.remoteAddress + ':' + socket.remotePort
+	);
+
+	socket.on('data', (data) => {
+		console.log('Mensaje recibido del cliente:', data.toString());
+		// Respondemos al cliente
+		const message = JSON.stringify({
+			sended: new Date().toISOString(),
+			message: `Saludos desde el servidor ${serverAddress.host}:${serverAddress.port}`,
+		});
+		socket.write(message);
+	});
+
+	socket.on('error', (err) => {
+		console.error('Error en el cliente:', err.message);
+	});
+});
+
+server.listen(0, serverAddress.host, () => {
+	serverAddress.port = server.address().port;
+	console.log(
+		`Servidor TCP escuchando en ${serverAddress.host}:${serverAddress.port}`
+	);
+});
+
+// Crear cliente y conectarse a servidor de contactos
+function connectToContactServer() {
+	const client = new net.Socket();
+
+	client.connect(remoteAddress, () => {
+		console.log(
+			'Conexión establecida con servidor el servidor de contactos.'
+		);
+		const message = JSON.stringify({
+			port: serverAddress.port,
+			host: serverAddress.host,
+		});
+		client.write(message);
+	});
+
+	client.on('data', (data) => {
+		console.log('Mensaje recibido desde el servidor:', data.toString());
+		const { status, message, nodes } = JSON.parse(data.toString());
+		if (status === 'OK') {
+			console.log(message);
+			if (params.nodes.length > 0) {
+				console.log('Enviando saludos a los nodos...');
+				handleHandshakes(nodes);
+			} else {
+				console.log(
+					'Este es el primer nodo registrado en el servidor de contactos!'
+				);
+			}
+		}
+
+		if (status === 'ERROR') {
+			console.log(message);
+		}
+	});
+
+	client.on('error', () => {
+		console.log(
+			'Error en la conexión con el servidor de contactos. Intentando reconectar...'
+		);
+		setTimeout(connectToContactServer, 10000);
+	});
+}
+
+// Envío de saludo a lista de nodos
+function handleHandshakes(nodes) {
+	nodes.map((node) => {
+		connectToNode(node);
+	});
+}
+
+// Crear cliente y conectarse al otro nodo
+function connectToNode(node) {
+	const client = new net.Socket();
+	client.connect(node, () => {
+		console.log('Conexión establecida con nodo ', node);
+		const message = JSON.stringify({
+			sended: new Date().toISOString(),
+			message: `Hola, soy un cliente tipo C. Conectado desde ${serverAddress.host}:${serverAddress.port}`,
+		});
+		client.write(message);
+	});
+
+	client.on('data', (data) => {
+		console.log('Mensaje recibido desde el nodo ', node);
+		console.log(data.toString());
+	});
+
+	client.on('error', (error) => {
+		console.error('Error en la conexión con nodo ', node);
+	});
+}
+
+connectToContactServer();
---
+++ TP1/ej7/contact-server.js
@@ -0,0 +1,98 @@
+const net = require('net');
+const http = require('http');
+
+const PORT = 3007;
+
+const nodes = [];
+
+const server = net.createServer((socket) => {
+	socket.on('data', (data) => {
+		console.log('Mensaje recibido del cliente:', data.toString());
+		try {
+			const params = JSON.parse(data.toString());
+
+			if (
+				!params.hasOwnProperty('port') ||
+				!params.hasOwnProperty('host')
+			) {
+				throw new Error('Parámetros de registro inválidos');
+			}
+
+			if (Number.isNaN(params.port)) {
+				throw new Error('El puerto debe ser un número');
+			}
+
+			if (params.host.length === 0) {
+				throw new Error('El host no puede estar vacío');
+			}
+
+			socket.write(
+				JSON.stringify({
+					status: 'OK',
+					message: 'Nodo registrado con éxito',
+					nodes: nodes,
+				})
+			);
+
+			nodes.forEach((node) => {
+				if (node.port === params.port && node.host === params.host) {
+					throw new Error('Nodo ya registrado');
+				}
+			});
+
+			nodes.push({ host: params.host, port: params.port });
+
+			socket.on('error', (err) => {
+				console.log(err);
+			});
+
+			socket.on('close', () => {
+				nodes.forEach((node) => {
+					try {
+						const client = new net.Socket();
+						client.connect(port, host, () => {
+							client.write('ping');
+						});
+					} catch (error) {
+						console.log(node, nodes);
+						nodes.splice(nodes.indexOf(node), 1);
+					}
+				});
+				console.log('Cliente desconectado.');
+			});
+		} catch (error) {
+			socket.write(
+				JSON.stringify({
+					status: 'ERROR',
+					message: error.message,
+				})
+			);
+		}
+	});
+});
+
+server.listen(PORT, () => {
+	console.log(`Servidor TCP escuchando en el puerto ${PORT}`);
+});
+
+server.on('connection', (req, res) => {
+	console.log('Cliente conectado');
+});
+
+const statusServer = http.createServer((req, res) => {
+	if (req.url === '/status') {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(
+			JSON.stringify({
+				service: 'Servidor de registro',
+				status: 'OK',
+				message: 'Servidor funcionando correctamente',
+			})
+		);
+	} else {
+		res.writeHead(404, { 'Content-Type': 'text/plain' });
+		res.end('Ruta no encontrada');
+	}
+});
+
+statusServer.listen(8087);
