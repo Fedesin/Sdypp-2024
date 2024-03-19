b822bdaad122b616552d22bc12533746fe1c77fa
Federico Simone <91753433+Fedesin@users.noreply.github.com>
2024-03-19 15:55:00 -0300

    Update server.js Ahora trabaja con UDP

--- TP1/ej2/server.js
+++ TP1/ej2/server.js
@@ -1,46 +1,26 @@
-const net = require('net');
-const http = require('http');
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
 const PORT = 3002;
 
-const server = net.createServer((socket) => {
-	socket.on('data', (data) => {
-		console.log('Mensaje recibido del cliente:', data.toString());
-		socket.write(
-			JSON.stringify({
-				message: 'Hola soy B. Mensaje recibido correctamente',
-				receivedMessage: data.toString(),
-			})
-		);
-	});
-	// Manejo del evento de error en el socket del cliente
-	socket.on('error', (err) => {
-		console.error('Error en el socket del cliente:', err.message);
-	});
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`Servidor UDP escuchando en ${address.address}:${address.port}`);
 });
 
-server.listen(PORT, () => {
-	console.log(`Servidor TCP escuchando en el puerto ${PORT}`);
+server.on('message', (message, remoteInfo) => {
+  console.log(`Mensaje recibido de ${remoteInfo.address}:${remoteInfo.port}:`, message.toString());
+
+  const response = `Mensaje recibido: ${message.toString()}`;
+  server.send(response, 0, response.length, remoteInfo.port, remoteInfo.address, (error) => {
+    if (error) {
+      console.error('Error al enviar respuesta:', error.message);
+    }
+  });
 });
 
-server.on('connection', (req, res) => {
-	console.log('Cliente conectado');
+server.on('close', () => {
+  console.log('Servidor UDP cerrado.');
 });
 
-const statusServer = http.createServer((req, res) => {
-	if (req.url === '/status') {
-		res.writeHead(200, { 'Content-Type': 'application/json' });
-		res.end(
-			JSON.stringify({
-				service: 'Servidor socket TCP',
-				status: 'OK',
-				message: 'Servidor funcionando correctamente',
-			})
-		);
-	} else {
-		res.writeHead(404, { 'Content-Type': 'text/plain' });
-		res.end('Ruta no encontrada');
-	}
-});
-
-statusServer.listen(8082);
+server.bind(PORT);
