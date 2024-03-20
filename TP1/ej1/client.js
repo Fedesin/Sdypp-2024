ca43dca2c4a16ed0310494ae77417a176b42c8d5
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-20 09:24:57 -0300

    fix: remueve intento de reconexion

--- TP1/ej1/client.js
+++ TP1/ej1/client.js
@@ -6,34 +6,19 @@ const options = {
 };
 
 const client = net.createConnection(options, () => {
-	console.log('Conectado al servidor');
-	waitForUserInput();
+	console.log('Conectado al servidor TCP');
+	client.write('Hola, soy A.');
 });
 
 client.on('close', () => {
-console.log('Conexión cerrada, intentando reconectar...');
-    // Intentar reconectar después de 1 segundo
-    setTimeout(() => {
-        client = net.createConnection(options, () => {
-            console.log('Conectado al servidor');
-            // Envía el saludo nuevamente al reconectar
-            client.write("Hola, servidor!");
-            waitForUserInput();
-        });
-    }, 1000);
+	console.log('Conexión cerrada con el servidor TCP');
+});
+
+client.on('error', (error) => {
+	console.error('Error en la conexión:', error.message);
+	client.destroy();
 });
 
 client.on('data', (data) => {
-	console.log(`\n Respuesta del servidor: ${data.toString()} \n`);
-	process.stdout.write('Ingrese otro mensaje para enviar al servidor: ');
+	console.log('Mensaje recibido de B:', data.toString());
 });
-
-// Función para esperar la entrada del usuario y enviarla al servidor
-function waitForUserInput() {
-	process.stdout.write('Ingrese un mensaje para enviar al servidor: ');
-
-	process.stdin.on('data', (data) => {
-		const message = data.toString().trim();
-		client.write(message);
-	});
-}
