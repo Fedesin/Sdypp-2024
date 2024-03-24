18ee415d211cf37e5caffbb810251b8953ae4cc5
Fedesin <fedesimone31@gmail.com>
2024-03-24 16:25:34 -0300

    cambie texto

--- TP1/ej1/client.js
+++ TP1/ej1/client.js
@@ -7,7 +7,7 @@ const options = {
 
 const client = net.createConnection(options, () => {
 	console.log('Conectado al servidor TCP');
-	client.write('Hola, soy A.');
+	client.write('Hola, soy Al pacino xd.');
 });
 
 client.on('close', () => {
