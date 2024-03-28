06465977e1690b7b97c245fe09e66658fa5d51c5
SimonDiLeoGIT <saimondiley@gmail.com>
2024-03-28 15:13:17 -0300

    comlpleta task-service

--- TP2/task-service/src/controllers/task.controller.js
+++ TP2/task-service/src/controllers/task.controller.js
@@ -1,9 +1,17 @@
 import { Task } from '../model/Task.js';
 
 export class TaskController {
+	
+
 	sum = (request, response) => {
 		const task = new Task();
-		const result = task.sum();
+
+		const body = request.body;
+
+		const params = body.params;
+
+		const result = task.sum(params);
 		response.end(JSON.stringify({ result }));
 	};
+
 }
