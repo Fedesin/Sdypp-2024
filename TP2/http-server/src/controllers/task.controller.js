1e7ae866bc3f60b923821631f4e8233e04e9df7a
SimonDiLeoGIT <saimondiley@gmail.com>
2024-03-28 15:56:27 -0300

    fix: fixing

--- TP2/http-server/src/controllers/task.controller.js
+++ TP2/http-server/src/controllers/task.controller.js
@@ -7,6 +7,10 @@ export class TaskController {
 
 		// Enviar petición HTTP para que la tarea se ejecute con los parámetros (task, params).
 		try {
+			
+			console.log("params: ", request.body)
+			console.log("task: ", task)
+			
 			const response = await fetch(`http://localhost:5000/task/${task}`, {
 				method: 'POST',
 				body: JSON.stringify({ params }),
