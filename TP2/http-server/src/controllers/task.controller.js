02f7f92cbf6d2ae5f8548dcaea00e7ee19b3aa1d
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-28 15:32:44 -0300

    agrega ejecución de tarea genérica

--- TP2/http-server/src/controllers/task.controller.js
+++ TP2/http-server/src/controllers/task.controller.js
@@ -1,6 +1,34 @@
 export class TaskController {
-	executeGenericTask = (request, response) => {
-		response.send('Task executed');
-		// TODO: Debe levantar el contendor docker de la tarea genérica, enviar una petición HTTP para que la tarea se ejecute y envíar el resultado en la response.
+	executeGenericTask = async (request, response) => {
+		const { body } = request;
+		const { image, task, params } = body;
+
+		// TODO: Levantar contenedor con la imagen (image)
+
+		// Enviar petición HTTP para que la tarea se ejecute con los parámetros (task, params).
+		try {
+			const response = await fetch(`http://localhost:5000/task/${task}`, {
+				method: 'POST',
+				body: JSON.stringify({ params }),
+			});
+
+			const data = await response.json();
+			const result = data.result;
+			response.status(200);
+			response.end(
+				JSON.stringify({
+					message: `Tarea ${task} ejecutada exitosamente`,
+					result,
+				})
+			);
+		} catch (error) {
+			response.status(400);
+			response.end(
+				JSON.stringify({
+					message: `Error al ejecutar la tarea ${task}`,
+					result,
+				})
+			);
+		}
 	};
 }
