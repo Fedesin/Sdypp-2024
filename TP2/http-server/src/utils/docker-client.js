12ff78af6660e6fd6106fe1aa4d1a04a11b24c9f
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-03-29 19:57:17 -0300

    remueve codigo comentado

--- TP2/http-server/src/utils/docker-client.js
+++ TP2/http-server/src/utils/docker-client.js
@@ -109,38 +109,4 @@ export class DockerClient {
 			);
 		});
 	}
-
-	// async getIPByName(containerName) {
-	// 	return new Promise((resolve, reject) => {
-	// 		this.docker.listContainers({ all: true }, (err, containers) => {
-	// 			if (err) {
-	// 				console.error('Error listing containers:', err);
-	// 				reject(err);
-	// 				return;
-	// 			}
-
-	// 			const container = containers.find((container) =>
-	// 				container.Names.includes('/' + containerName)
-	// 			);
-	// 			if (!container) {
-	// 				reject(
-	// 					new Error(
-	// 						`Container with name ${containerName} not found`
-	// 					)
-	// 				);
-	// 				return;
-	// 			}
-
-	// 			const containerObj = this.docker.getContainer(container.Id);
-	// 			containerObj.inspect((err, data) => {
-	// 				if (err) {
-	// 					console.error('Error inspecting container:', err);
-	// 					reject(err);
-	// 					return;
-	// 				}
-	// 				resolve(data.NetworkSettings.IPAddress);
-	// 			});
-	// 		});
-	// 	});
-	// }
 }
