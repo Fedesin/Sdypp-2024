7ed1f8b5cab3025dfb8ed47990a9bb3c7775daab
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-01 11:35:41 -0300

    agrega comando para ejecución del contenedor

--- TP2/vm-gcp/init.sh
+++ TP2/vm-gcp/init.sh
@@ -7,4 +7,6 @@
 curl -fsSL https://get.docker.com/ -o get-docker.sh
 sudo sh get-docker.sh
 
+docker network create generic-task-network
+
 docker run -p 3000:3000 --name http-server --network generic-task-network -v /var/run/docker.sock:/var/run/docker.sock fedesin31/tp2-http-server
