e2104c944aa7abac02ef0584f970de18316e6709
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-21 11:38:43 -0300

    agrega regla de firewall para habilitar https

--- TP3/ej2-terraform/networking.tf
+++ TP3/ej2-terraform/networking.tf
@@ -22,6 +22,18 @@ resource "google_compute_firewall" "allow_http" {
   }
 }
 
+resource "google_compute_firewall" "allow_https" {
+  name          = "allow-https"
+  network       = "default"
+  target_tags   = ["allow-https"]
+  source_ranges = ["0.0.0.0/0"]
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+}
+
 resource "local_file" "instance_ip" {
   content         = google_compute_instance.vm_instance[0].network_interface[0].access_config[0].nat_ip
   filename        = "temp/instance_ip.txt"
