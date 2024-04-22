83793e837420462d3980ed85c83efdec72b29c6b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-22 18:18:23 -0300

    completa HIT 3

--- TP3/ej3-node-sdk/app.js
+++ TP3/ej3-node-sdk/app.js
@@ -2,6 +2,8 @@ const { InstancesClient } = require('@google-cloud/compute');
 const fs = require('fs');
 const readline = require('readline');
 
+let compute = null;
+
 async function createClient(projectId, credentials) {
 	try {
 		return new InstancesClient({
@@ -10,6 +12,80 @@ async function createClient(projectId, credentials) {
 		});
 	} catch (error) {
 		console.error('Error al crear el cliente GCP:', error);
+	}
+}
+
+// Función para detener una instancia
+async function stopInstance(projectId, zone, instanceId) {
+	try {
+		await compute.stop({
+			project: projectId,
+			zone: zone,
+			instance: instanceId,
+		});
+		console.log(`Instancia ${instanceId} detenida correctamente.`);
+	} catch (error) {
+		console.error('Error al detener la instancia:', error);
+	}
+}
+
+// Función para iniciar una instancia
+async function startInstance(projectId, zone, instanceId) {
+	try {
+		await compute.start({
+			project: projectId,
+			zone: zone,
+			instance: instanceId,
+		});
+		console.log(`Instancia ${instanceId} iniciada correctamente.`);
+	} catch (error) {
+		console.error('Error al iniciar la instancia:', error);
+	}
+}
+
+// Función para eliminar una instancia
+async function deleteInstance(projectId, zone, instanceId) {
+	try {
+		await compute.delete({
+			project: projectId,
+			zone: zone,
+			instance: instanceId,
+		});
+		console.log(`Instancia ${instanceId} eliminada correctamente.`);
+	} catch (error) {
+		console.error('Error al eliminar la instancia:', error);
+	}
+}
+
+// Función para listar las instancias de forma estética
+async function listInstances(projectId, zone) {
+	try {
+		const [instances] = await compute.list({
+			project: projectId,
+			zone: zone,
+		});
+
+		console.log(
+			'\n====================== INSTANCIAS ===========================\n'
+		);
+
+		if (instances.length === 0) {
+			console.log("No hay instancias de VM's existentes en el proyecto");
+			return;
+		}
+
+		console.log(
+			' Número | ID                 | Nombre               | Estado'
+		);
+		instances.forEach(({ id, name, status }, index) => {
+			console.log(
+				` ${(index + 1).toString().padEnd(6)} | ${id.padEnd(
+					18
+				)} | ${name.padEnd(18)} | ${status}`
+			);
+		});
+	} catch (error) {
+		console.error('Error al listar las instancias:', error);
 	}
 }
 
@@ -58,34 +134,48 @@ function prompt(question) {
 			process.exit(1);
 		}
 
-		const client = await createClient(projectId, credentials);
+		compute = await createClient(projectId, credentials);
 
 		const zone = 'us-east1-b';
 
-		const [instances] = await client.list({
-			project: projectId,
-			zone,
-		});
+		let option = -1;
+		while (option !== 0) {
+			console.log('\nMenú:');
+			console.log('1. Listar instancias');
+			console.log('2. Detener instancia');
+			console.log('3. Iniciar instancia');
+			console.log('4. Eliminar instancia');
+			console.log('0. Salir \n');
 
-		console.log(
-			'\n====================== INSTANCIAS ===========================\n'
-		);
-
-		if (instances.length === 0) {
-			console.log("No hay instancias de VM's existentes en el proyecto");
-			process.exit(0);
+			option = await prompt('Seleccione una opción: ');
+			switch (option) {
+				case '1':
+					await listInstances(projectId, zone);
+					break;
+				case '2':
+					const stopInstanceId = await prompt(
+						'Ingrese el ID de la instancia a detener: '
+					);
+					await stopInstance(projectId, zone, stopInstanceId);
+					break;
+				case '3':
+					const startInstanceId = await prompt(
+						'Ingrese el ID de la instancia a iniciar: '
+					);
+					await startInstance(projectId, zone, startInstanceId);
+					break;
+				case '4':
+					const deleteInstanceId = await prompt(
+						'Ingrese el ID de la instancia a eliminar: '
+					);
+					await deleteInstance(projectId, zone, deleteInstanceId);
+					break;
+				case '0':
+					process.exit(0);
+				default:
+					console.log('Opción no válida.');
+			}
 		}
-
-		console.log(
-			' Número | ID                  | Nombre               | Estado'
-		);
-		instances.forEach(({ id, name, status }, index) => {
-			console.log(
-				` ${(index + 1).toString().padEnd(6)} | ${id.padEnd(
-					18
-				)} | ${name.padEnd(18)} | ${status}`
-			);
-		});
 	} catch (error) {
 		console.error(
 			'Error al cargar las credenciales desde el archivo:',
