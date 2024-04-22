8f9d351fbe38a36a0c0c73e20882af9508d1b262
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-22 17:31:14 -0300

    refactor

--- TP3/ej1-node-sdk/app.js
+++ TP3/ej1-node-sdk/app.js
@@ -2,61 +2,6 @@ const { InstancesClient } = require('@google-cloud/compute');
 const fs = require('fs');
 const readline = require('readline');
 
-if (process.argv.length !== 3) {
-	console.log(
-		'Comando invalido. Utilice node app.js <path-to-credentials.json>'
-	);
-	process.exit(1);
-}
-
-const credentialsPath = process.argv.slice(2)[0];
-
-if (!credentialsPath) {
-	console.error('Debes proporcionar la ruta al archivo credentials.json');
-	process.exit(1);
-}
-
-const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout,
-});
-
-try {
-	const credentials = JSON.parse(fs.readFileSync(credentialsPath, 'utf-8'));
-
-	rl.question(
-		'Por favor ingresa el ID del proyecto de GCP: ',
-		(projectId) => {
-			projectId = projectId.trim();
-
-			if (!projectId) {
-				console.error('Debes proporcionar un ID de projecto GCP');
-				process.exit(1);
-			}
-
-			rl.question(
-				'Por favor ingresa la cantidad de instancias que deseas crear: ',
-				(numInstances) => {
-					numInstances = parseInt(numInstances.trim());
-
-					if (!numInstances || numInstances < 1) {
-						console.error(
-							'Debes proporcionar un número de instancias válido'
-						);
-						process.exit(1);
-					}
-
-					createInstances(projectId, credentials, numInstances);
-					rl.close();
-				}
-			);
-		}
-	);
-} catch (error) {
-	console.error('Error al cargar las credenciales desde el archivo:', error);
-	process.exit(1);
-}
-
 async function createInstances(projectId, credentials, numInstances) {
 	try {
 		const instancesClient = new InstancesClient({
@@ -101,9 +46,73 @@ async function createInstances(projectId, credentials, numInstances) {
 			});
 
 			console.log(`Instancia ${instanceName} en la zona ${zone} creada.`);
-			console.log(`Operación en progreso: ${operation.name}`);
 		}
 	} catch (error) {
 		console.error('Error al crear las instancias:', error);
 	}
 }
+
+// Función para leer una línea desde la entrada estándar
+function prompt(question) {
+	const rl = readline.createInterface({
+		input: process.stdin,
+		output: process.stdout,
+	});
+
+	return new Promise((resolve) => {
+		rl.question(question, (answer) => {
+			rl.close();
+			resolve(answer);
+		});
+	});
+}
+
+if (process.argv.length !== 3) {
+	console.log(
+		'Comando invalido. Utilice node app.js <path-to-credentials.json>'
+	);
+	process.exit(1);
+}
+
+(async () => {
+	const credentialsPath = process.argv.slice(2)[0];
+
+	if (!credentialsPath) {
+		console.error('Debes proporcionar la ruta al archivo credentials.json');
+		process.exit(1);
+	}
+
+	try {
+		const credentials = JSON.parse(
+			fs.readFileSync(credentialsPath, 'utf-8')
+		);
+
+		let projectId = await prompt(
+			'Por favor ingresa el ID del proyecto de GCP: '
+		);
+		projectId = projectId.trim();
+
+		if (!projectId) {
+			console.error('Debes proporcionar un ID de projecto GCP');
+			process.exit(1);
+		}
+
+		let instances = await prompt(
+			'Por favor ingresa la cantidad de instancias que deseas crear: '
+		);
+		instances = parseInt(instances.trim());
+
+		if (!instances || instances < 1) {
+			console.error('Debes proporcionar un número de instancias válido');
+			process.exit(1);
+		}
+
+		createInstances(projectId, credentials, instances);
+	} catch (error) {
+		console.error(
+			'Error al cargar las credenciales desde el archivo:',
+			error
+		);
+		process.exit(1);
+	}
+})();
