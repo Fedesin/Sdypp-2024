e815dc3f4a997ab3eae66e390b3de21fe08d6c44
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-04-29 17:56:06 -0300

    agrega un parámetro en la ruta para definir la cantidad de partes en la que se dividirá la imagen

--- TP4/ej1/parte2/server.py
+++ TP4/ej1/parte2/server.py
@@ -7,9 +7,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/api/split", methods=['POST'])
-def split():
-    if request.method == 'POST':        
+@app.route("/api/split/<int:num_fragments>", methods=['POST'])
+def sobel(num_fragments):
+    if request.method == 'POST':    
+        # Verificar que num_fragments esté dentro del rango permitido
+        if num_fragments < 1 or num_fragments > 10:
+            return "Bad request: el número de fragmentos debe estar entre 1 y 10", 400
+            
         # Verifica si hay un archivo adjunto de imagen en la solicitud
         if 'image' in request.files:
             # Obtén el archivo adjunto de la imagen
@@ -26,14 +30,14 @@ def split():
             
             # Verifica si la imagen se ha cargado correctamente
             if image is None:
-                return "Error al procesar la imagen", 400
+                return "Bad request: error al procesar la imagen", 400
             
             # Aplica el filtro Sobel a la imagen
-            fragments = split_image(image, 2)
+            fragments = split_image(image, num_fragments)
             join_image(fragments)
             # Devuelve la imagen procesada
             # return send_file(imagen_sobel, mimetype='image/png', as_attachment=True)
-            return jsonify("parts: 2")
+            return "Imagen dividida en {} partes".format(num_fragments), 200
     else:
         return "Método no permitido o imagen no adjunta", 405  # Método no permitido o imagen no adjunta
 
