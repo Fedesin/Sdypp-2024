241df58f57dcd1385d0e65e932b11c818a360449
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-12 15:10:24 -0300

    implementación join-service

--- TP4/ej3/join-service/src/utils/join.py
+++ TP4/ej3/join-service/src/utils/join.py
@@ -1,26 +1,16 @@
-import uuid
 import cv2
 import os
 import numpy as np
 
 
-def generate_random_name(length=8):
-    random_name = str(uuid.uuid4()) + '.png'
-    return random_name
-
-
-def join_image(original_fragments, sobel_fragments):
-    fragments = []
-
-    for fragment in original_fragments:
-        fragments.append(sobel_fragments[fragment]["sobel_fragment_path"])
-
+def join_image(fragments, task_id):
     # Cargar los fragmentos de imagen
-    fragment_images = [cv2.imread(fragment) for fragment in fragments]
+    fragment_images = [cv2.imread(os.path.join(
+        os.getcwd(), "tmp", "fragments", fragment)) for fragment in fragments]
 
     # Obtener el tamaño de la primera imagen para crear la imagen final
     height, width, channels = fragment_images[0].shape
-    num_fragments = len(fragment_images)
+    num_fragments = len(fragments)
 
     # Crear una imagen vacía para unir los fragmentos
     joined_image = np.zeros(
@@ -31,7 +21,8 @@ def join_image(original_fragments, sobel_fragments):
         fragment = fragment_images[i]
         joined_image[i*height: (i+1)*height, :] = fragment
 
-    random_name = generate_random_name()
-    path = os.path.join('tmp', random_name)
+    if not os.path.exists('tmp/results'):
+        os.makedirs('tmp/results')
+
+    path = os.path.join('tmp', "results", f"{task_id}.png")
     cv2.imwrite(path, joined_image)
-    return path
