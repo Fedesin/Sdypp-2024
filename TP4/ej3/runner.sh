5d5e48040d3835d5dffb0777ea4f2828fefefe08
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-12 16:45:40 -0300

    actualiza runner.sh

--- TP4/ej3/runner.sh
+++ TP4/ej3/runner.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+cd terraform
+sh create.sh
+cd ..
+docker compose up -d
 cd ../Images
-sleep 5
+echo ""
+echo "Iniciando tarea sobel. Espere unos segundos"
+echo "Recuerde copiar y resguardar el TASK_ID para luego obtener el resultado de la operación \n"
+echo ""
+
+sleep 15
+
 curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" -w '\nTiempo total: %{time_total}s\n' http://localhost:5001/api/sobel
