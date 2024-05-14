ae5cbbe477904874ccbcc869b4111bf96503d2c7
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-14 14:59:52 -0300

    fix entry-server (deployment + service)

--- TP4/ej3/kubernetes/apply.sh
+++ TP4/ej3/kubernetes/apply.sh
@@ -11,15 +11,16 @@ kubectl apply -f split-service.yml
 sleep 10
 
 # Obtener el nombre del deployment entry-server deployment.
-pod_name=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}')
-# service_name=$(kubectl get services -o=jsonpath='{.items[0].metadata.name}')
+# pod_name=$(kubectl get pods -o=jsonpath='{.items[0].metadata.name}')
+service_name=$(kubectl get services -o=jsonpath='{.items[0].metadata.name}')
 
-if [ -z "$pod_name" ]; then
-    echo "No se encontraron pods."
+if [ -z "$service_name" ]; then
+    echo "No se encontraron servicios"
     exit 1
+else 
+    echo $service_name
 fi
 
-echo $pod_name
+echo $service_name
 
-kubectl port-forward $pod_name 5001:5000
-# kubectl port-forward svc/$service_name 5001:5000
+kubectl port-forward service/entry-server 5000:5000
