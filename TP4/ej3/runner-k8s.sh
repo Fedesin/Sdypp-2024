36a64ee153ef4a6f1f4f3f6dfd20d7533292a69e
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-19 18:12:26 -0300

    (down) fix join service

--- TP4/ej3/runner-k8s.sh
+++ TP4/ej3/runner-k8s.sh
@@ -4,8 +4,9 @@ echo ""
 echo "Iniciando tarea sobel!"
 echo "Recuerde copiar y resguardar el TASK_ID para luego obtener el resultado de la operación \n"
 echo "Espere unos segundos... \n"
-sleep 15
+sleep 5
 
 
-curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image5.jpg" -w '\nTiempo total: %{time_total}s\n' http://34.23.234.167:5000/api/sobel
-# curl http://34.23.234.167:5000/api/status
+curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" -w '\nTiempo total: %{time_total}s\n' http://34.73.254.137:5000/api/sobel
+# curl http://34.73.254.137:5000/api/status
+
