6f910883e8f913194f165f1eeea9dae0eb558c5b
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-20 12:42:05 -0300

    (down)

--- TP4/ej3/runner-k8s.sh
+++ TP4/ej3/runner-k8s.sh
@@ -7,6 +7,6 @@ echo "Espere unos segundos... \n"
 sleep 5
 
 
-curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" -w '\nTiempo total: %{time_total}s\n' http://34.73.254.137:5000/api/sobel
-# curl http://34.73.254.137:5000/api/status
-
+curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" -w '\nTiempo total: %{time_total}s\n' http://34.75.109.87:5000/api/sobel
+# curl http://34.75.109.87:5000/api/status
+# http://34.75.109.87:5000/api/result/
