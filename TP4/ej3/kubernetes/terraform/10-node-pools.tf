f2988b5f6d4081c2496e807a18941b1db646f946
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-21 20:59:52 -0300

    actualiza machine types

--- TP4/ej3/kubernetes/terraform/10-node-pools.tf
+++ TP4/ej3/kubernetes/terraform/10-node-pools.tf
@@ -21,7 +21,7 @@ resource "google_container_node_pool" "applications" {
 
   node_config {
     preemptible  = false
-    machine_type = "e2-medium" 
+    machine_type = "e2-highcpu-4" # e2-highcpu-8
 
     labels = {
       role = "applications"
@@ -58,7 +58,7 @@ resource "google_container_node_pool" "services" {
 
   node_config {
     preemptible  = false
-    machine_type = "c2-standard-4" 
+    machine_type = "e2-standard-4" # c2-standard-4 
 
     labels = {
       role = "services"
