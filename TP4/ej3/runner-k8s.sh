386ba6ce721164534f6bc652ebb776639a06c0cd
Matias Gimenez <matiasgimenez.dev@gmail.com>
2024-05-25 19:00:57 -0300

    (up)

--- TP4/ej3/runner-k8s.sh
+++ TP4/ej3/runner-k8s.sh
@@ -5,14 +5,18 @@ echo "Iniciando tarea sobel!"
 echo "Recuerde copiar y resguardar el TASK_ID para luego obtener el resultado de la operación \n"
 echo "Espere unos segundos... \n"
 sleep 5
+
 current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
 echo "Current date and time: $current_date_time"
-# curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image3.jpg" http://34.172.98.204:5000/api/sobel
-# curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image4.jpg" http://34.172.98.204:5000/api/sobel
-# curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" http://34.172.98.204:5000/api/sobel
-curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image8.jpg" http://34.172.98.204:5000/api/sobel
-
+curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" http://35.222.119.63:5000/api/sobel
+current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Current date and time: $current_date_time"
+curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" http://35.222.119.63:5000/api/sobel
+current_date_time=$(date +"%Y-%m-%d %H:%M:%S")
+echo "Current date and time: $current_date_time"
+curl -X POST -H "Content-Type: multipart/form-data" -F "image=@Image6.jpg" http://35.222.119.63:5000/api/sobel
 
 
 # curl http://34.29.25.48:5000/api/status
 # http://34.29.25.48:5000/api/result/6a083421-1af9-4e48-a383-c7a028ca730e
+
